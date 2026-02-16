360a5da85898035da01305786d0971280a5cf010
SHEIKABDULLAHPM <sheikabdullahpeer@gmail.com>
2026-02-16 20:15:10 +0530

    chnage

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export', // Enable static exports
-  basePath: '/birthday-page', // Use your repository name
+  basePath: isProd ? '/nanban' : '', // Use your repository name
+  assetPrefix: isProd ? '/nanban/' : '', // Required for static assets on GitHub Pages
   images: {
     unoptimized: true, // Required for static export
   },
+  trailingSlash: true, // Helps with GitHub Pages routing
 };
 
 export default nextConfig;
